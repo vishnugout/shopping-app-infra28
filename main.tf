ce5d43c27a4f04f836c36f38d6d44b9ec0447421
victor <noah@ihnetworks.com>
2023-12-28 17:20:41 +0000

    port removed

--- main.tf
+++ main.tf
@@ -32,15 +32,6 @@ resource "aws_security_group" "http_access" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  ingress {
-    from_port        = 9090
-    to_port          = 9090
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-
 
   ingress {
     from_port        = 443
